a668a760024ba1f9ee805d549f5fdb34113e6db9
saikrishnakingtyle <77505364+saikrishnakingtyle@users.noreply.github.com>
2024-02-04 00:04:00 +0530

    added the header login detect feature to change the options in header after logging in.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -17,6 +17,7 @@ export class HeaderComponent implements OnInit ,OnChanges {
     teamListData : any = JSON.parse(<any>localStorage.getItem('teamList'));
     ngOnChanges(changes: SimpleChanges): void {
       console.log("The changes has been made in header.")
+      console.log("login data in ngOnChanges : ", this.login)
       this.organisationListData = JSON.parse(<any>localStorage.getItem('organisationList'));
       this.teamListData = JSON.parse(<any>localStorage.getItem('teamList'));
     }
@@ -24,15 +25,22 @@ export class HeaderComponent implements OnInit ,OnChanges {
       this.loggedInUserData = JSON.parse(<any>localStorage.getItem('loggedInUser'));
       this.organisationListData = JSON.parse(<any>localStorage.getItem('organisationList'));
       this.teamListData = JSON.parse(<any>localStorage.getItem('teamList'));
-      if (this.loggedInUserData !== undefined || this.loggedInUserData !== null) {
+      console.log("the value of this.loggedInUserData is this : ", this.loggedInUserData);
+      if (this.loggedInUserData !== undefined && this.loggedInUserData !== null) {
         this.login = true;
+        console.log("Into the if : ")
+      } else {
+        this.login = false;
+        console.log("Into the else : ")
       }
+      console.log("login data in ngOnInit : ", this.login)
     }
     onClick(){
       this.router.navigate(['/'], { relativeTo: this.route });
     }
     getInputValue ($event: any) {
       console.log("The event value is this : ", $event);
+      console.log("login data in getInputValue : ",this.login)
       this.organisationListData = JSON.parse(<any>localStorage.getItem('organisationList'));
       this.teamListData = JSON.parse(<any>localStorage.getItem('teamList'));
     } 

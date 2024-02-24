01a8b836f8f0fcfa6b1cece1c5c56ca17cfdfc87
saikrishnakingtyle <77505364+saikrishnakingtyle@users.noreply.github.com>
2024-02-24 23:17:55 +0530

    Fixed the drop down

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -29,6 +29,13 @@ export class HeaderComponent implements OnInit ,OnChanges {
         currentOrganisation : data.textContent
       }
       localStorage.setItem('currentOrganisationTeamRef', JSON.stringify(organisationTeamMapping));
+      this.teamListData = JSON.parse(<any>localStorage.getItem('teamList'));
+      console.log("The teamlist before filter is this : ", this.teamListData)
+      this.teamListData = this.teamListData.filter((data : any)=> {
+        console.log("The data.organisationRef is this : ",data.organisationRef, " and the organsiationTeamMapping.currentOrganisation : ", organisationTeamMapping.currentOrganisation)
+        return data.organisationRef == organisationTeamMapping.currentOrganisation
+      })
+      console.log("The teamlist after filter is this : ", this.teamListData)
       if (this.accountType !== "Personal account"){
         // this.isPersonalAccount = false;
       }
@@ -39,6 +46,13 @@ export class HeaderComponent implements OnInit ,OnChanges {
       console.log("The data.textContent is this : " ,data.textContent);
       this.team = data.textContent;
       let organisationTeamMapping = JSON.parse(<any>localStorage.getItem('currentOrganisationTeamRef'));
+      this.teamListData = JSON.parse(<any>localStorage.getItem('teamList'));
+      console.log("The teamlist before filter is this : ", this.teamListData)
+      this.teamListData = this.teamListData.filter((data : any)=> {
+        console.log("Into the updateTeam the data.organisationRef is this : ",data.organisationRef, " and the organsiationTeamMapping.currentOrganisation : ", organisationTeamMapping.currentOrganisation)
+        return data.organisationRef == organisationTeamMapping.currentOrganisation
+      })
+      // console.log('The team data is this : ', organisationTeamMapping)
       organisationTeamMapping["currentTeam"] = this.team;
       localStorage.setItem('currentOrganisationTeamRef', JSON.stringify(organisationTeamMapping));
       if (this.accountType !== "Personal account"){
@@ -52,6 +66,11 @@ export class HeaderComponent implements OnInit ,OnChanges {
       this.organisationListData = JSON.parse(<any>localStorage.getItem('organisationList'));
       this.team = this.organisationListData.currentTeam || "My tasks";
       this.teamListData = JSON.parse(<any>localStorage.getItem('teamList'));
+      let organisationTeamMapping = JSON.parse(<any>localStorage.getItem('currentOrganisationTeamRef'));
+      this.teamListData = this.teamListData.filter((data : any)=> {
+        console.log("The data.organisationRef is this : ",data.organisationRef, " and the organsiationTeamMapping.currentOrganisation : ", organisationTeamMapping.currentOrganisation)
+        return data.organisationRef == organisationTeamMapping.currentOrganisation
+      })
     }
     ngOnInit() {
       this.loggedInUserData = JSON.parse(<any>localStorage.getItem('loggedInUser'));
@@ -61,6 +80,12 @@ export class HeaderComponent implements OnInit ,OnChanges {
       this.team = organisationTeamMapping?.currentTeam || "My tasks";
       this.accountType = organisationTeamMapping?.currentOrganisation || "Personal account";
       this.teamListData = JSON.parse(<any>localStorage.getItem('teamList'));
+      console.log("The teamlist before filter is this : ", this.teamListData)
+      this.teamListData = this.teamListData.filter((data : any)=> {
+        console.log("The data.organisationRef is this : ",data.organisationRef, " and the organsiationTeamMapping.currentOrganisation : ", organisationTeamMapping.currentOrganisation)
+        return data.organisationRef == organisationTeamMapping.currentOrganisation
+      })
+      console.log("The team list is this : ", this.teamListData)
       console.log("the value of this.loggedInUserData is this : ", this.loggedInUserData);
       console.log("The team is this : ", this.team)
       if (this.loggedInUserData !== undefined && this.loggedInUserData !== null) {
@@ -81,5 +106,12 @@ export class HeaderComponent implements OnInit ,OnChanges {
       this.organisationListData = JSON.parse(<any>localStorage.getItem('organisationList'));
       this.team = this.organisationListData.currentTeam || "My tasks";
       this.teamListData = JSON.parse(<any>localStorage.getItem('teamList'));
+      console.log("The teamlist before filter is this : ", this.teamListData)
+      let organisationTeamMapping = JSON.parse(<any>localStorage.getItem('currentOrganisationTeamRef'));
+      this.teamListData = this.teamListData.filter((data : any)=> {
+        console.log("The data.organisationRef is this : ",data.organisationRef, " and the organsiationTeamMapping.currentOrganisation : ", organisationTeamMapping.currentOrganisation)
+        return data.organisationRef == organisationTeamMapping.currentOrganisation
+      })
+      console.log("The team list is this : ", this.teamListData)
     } 
 }

40a75f70e00e711f0ceedf43d01396a5fa7f7f78
saikrishnakingtyle <77505364+saikrishnakingtyle@users.noreply.github.com>
2024-04-12 16:25:44 +0530

    fixed data of related user

--- src/app/dependancy/apiService.service.ts
+++ src/app/dependancy/apiService.service.ts
@@ -10,7 +10,7 @@ import { catchError, map } from 'rxjs/operators';
 @Injectable()
 export class ApiCall {
     constructor(private api : HttpClient) { };
-    apiUrl = 'http://localhost:3000';
+    apiUrl = 'http://backend.tasktodo.app';
     taskListApi(payload : any = null){
         return new Promise((res,rej)=>{
             let currentUser = JSON.parse(<any>localStorage.getItem('currentUser'));
